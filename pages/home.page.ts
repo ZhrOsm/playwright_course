cffc42604b9a0a07f7f084672507c6bb76802997
zhr.osmanogullari@gmail.com <zhr.osmanogullari@gmail.com>
2024-12-09 17:47:20 +0100

    4

---
+++ pages/home.page.ts
@@ -0,0 +1,29 @@
+import {Page,Locator} from '@playwright/test';
+
+class HomePage{
+    page: Page;
+    getstartedButton: Locator;
+    headingText: Locator;
+    homeText: Locator;
+    searchIcon: Locator;
+    navLinks: Locator;
+    constructor(page: Page){
+     this.page = page;
+     this.getstartedButton = page
+        .locator('#get-started')
+     this.headingText = page.locator('text=Think different. Make different.')
+     this.homeText = page.locator('#zak-primary-menu:has-text("Home")')
+     this.searchIcon =page.locator("//div[@class='zak-header-actions zak-header-actions--desktop']//a[@class='zak-header-search__toggle']")
+     this.navLinks = page.locator('#zak-primary-menu li[id*=menu]')
+    }
+
+    async navigate (){
+        await this.page.goto('https://practice.sdetunicorns.com/');
+    }
+
+    async getNavLinksText(){
+        return this.navLinks.allInnerTexts
+    }
+}
+
+export default HomePage;
